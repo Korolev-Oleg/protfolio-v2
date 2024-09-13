77d6f3b717c552225fe570b2d44801e4368a10af
Oleg Korolev <ok@hustn.cn>
2024-09-13 15:29:21 +0300

    update meta

---
+++ src/components/metainfo.vue
@@ -0,0 +1,10 @@
+<template>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+const title = ref("Portfolio | Oleg Korolev | FullStack Web Developer")
+const description = ref("Oleg Korolev is a FullStack Web Developer with a focus on Frontend Development. He is passionate about creating beautiful and functional websites.")
+const imageUri = ref("https://github.com/Korolev-Oleg/portfolio-v2/blob/main/src/components/about/asserts/profilePhoto1.png?raw=true")
+const url = ref("https://olegkorolev.com")
+</script>
