fa7b9467780cde0b20cd792a1d08e157f2ef9364
Oleg Korolev <ok@hustn.cn>
2024-10-14 19:02:10 +0300

    update gh-pages

--- src/components/Header/icons/VueIcon.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <svg width="19" height="17" viewBox="0 0 19 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M15.539 0.978439L9.71659 11.0628L3.89422 0.978439H0.597412L9.71659 16.774L18.8358 0.978439H15.539Z"
-          fill="black"/>
-  </svg>
-</template>
--- src/components/Service/ChevronDownIcon.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <svg width="17" height="11" viewBox="0 0 17 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M2.25 2.375L8.5 8.625L14.75 2.375" stroke="#C6C6C6" stroke-width="4" stroke-linecap="round"
-          stroke-linejoin="round"/>
-  </svg>
-</template>
